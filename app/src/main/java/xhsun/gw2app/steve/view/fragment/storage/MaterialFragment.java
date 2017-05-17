bd722cf2a750de6bc553f0007eeb5a8dd7e1a706
xhsun <xhsun@ucalgary.ca>
2017-05-17 00:00:34 -0600

    template for material fragment created

---
+++ app/src/main/java/xhsun/gw2app/steve/view/fragment/storage/MaterialFragment.java
@@ -0,0 +1,76 @@
+package xhsun.gw2app.steve.view.fragment.storage;
+
+import android.support.v4.app.Fragment;
+
+import java.util.Set;
+
+import xhsun.gw2app.steve.backend.data.AbstractData;
+import xhsun.gw2app.steve.backend.data.AccountInfo;
+import xhsun.gw2app.steve.backend.util.storage.StorageTabFragment;
+import xhsun.gw2app.steve.backend.util.vault.VaultHeader;
+import xhsun.gw2app.steve.backend.util.vault.VaultType;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * TODO layout, onCreateView, and fill all overrides
+ *
+ * @author xhsun
+ * @since 2017-05-16
+ */
+
+public class MaterialFragment extends StorageTabFragment {
+
+	public MaterialFragment() {
+		super(VaultType.MATERIAL);
+	}
+
+	@Override
+	public void updateData(AbstractData data) {
+
+	}
+
+	@Override
+	public void refreshData(AbstractData data) {
+
+	}
+
+	@Override
+	public void processChange(Set<AccountInfo> preference) {
+
+	}
+
+	@Override
+	public boolean shouldLoad() {
+		return false;
+	}
+
+	@Override
+	protected void onRefresh() {
+
+	}
+
+	@Override
+	protected VaultHeader generateContent() {
+		return null;
+	}
+
+	@Override
+	protected void displayAccount(VaultHeader header) {
+
+	}
+
+	@Override
+	protected boolean checkAvailability() {
+		return false;
+	}
+
+	@Override
+	protected boolean isAllRefreshed() {
+		return false;
+	}
+
+	@Override
+	public void onUpdateEmptyView(int size) {
+
+	}
+}
